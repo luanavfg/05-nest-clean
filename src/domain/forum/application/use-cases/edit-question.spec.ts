8356dc0cd66a1c89071fe23e78265b672819aaa1
luanavfg <luanavefago1@gmail.com>
2024-11-06 09:56:47 -0300

    config: get domain layer from project 04

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,96 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { makeQuestion } from 'test/factories/make-question'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from './edit-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let editQuestion: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+
+    editQuestion = new EditQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionAttachmentsRepository,
+    )
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    )
+
+    await editQuestion.execute({
+      questionId: newQuestion.id.toValue(),
+      authorId: 'author-1',
+      content: 'New content',
+      title: 'New title',
+      attachmentsIds: ['1', '3'],
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      content: 'New content',
+      title: 'New title',
+    })
+
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toEqual([
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('3'),
+      }),
+    ])
+  })
+
+  it('should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    const result = await editQuestion.execute({
+      questionId: newQuestion.id.toValue(),
+      authorId: 'author-2',
+      content: 'New content',
+      title: 'New title',
+      attachmentsIds: [],
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
+
+export interface AnswerCommentProps extends CommentProps {
+  answerId: UniqueEntityID
+}
+
+export class AnswerComment extends Comment<AnswerCommentProps> {
+  get answerId() {
+    return this.props.answerId
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    // TODO: send notification when there is new comment
+    const answerComment = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return answerComment
+  }
+}
