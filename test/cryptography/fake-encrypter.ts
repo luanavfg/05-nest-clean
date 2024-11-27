d48291e8ac821228f3dce9f22852cf9324af504f
luanavfg <luanavefago1@gmail.com>
2024-11-27 09:25:57 -0300

    feat: add tests to authenticate and register student

--- test/cryptography/fake-encrypter.ts
+++ test/cryptography/fake-encrypter.ts
@@ -1,6 +1,6 @@
 import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
 
-export class FakerEncrypter implements Encrypter {
+export class FakeEncrypter implements Encrypter {
   async encrypt(payload: Record<string, unknown>): Promise<string> {
     return JSON.stringify(payload)
   }
