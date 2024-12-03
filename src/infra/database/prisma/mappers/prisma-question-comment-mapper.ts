adf3e0dbbab5ea024ebcfc778fc3dd940d1266b7
Luana Vefago dos Santos <luana.santos@dynamox.net>
2024-12-03 09:15:10 -0300

    feat: add mappers

---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,34 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Comment as PrismaComment, Prisma } from '@prisma/client'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type')
+    }
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        questionId: new UniqueEntityID(raw.questionId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+
+  static toPrisma(
+    questionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      content: questionComment.content,
+      authorId: questionComment.authorId.toString(),
+      questionId: questionComment.questionId.toString(),
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    }
+  }
+}
