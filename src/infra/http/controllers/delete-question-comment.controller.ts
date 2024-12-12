cf70a98bc3f4f5f125d7074acde4aa6ca469b99b
Luana Vefago dos Santos <luana.santos@dynamox.net>
2024-12-12 08:52:11 -0300

    feat: delete question comment controller

---
+++ src/infra/http/controllers/delete-question-comment.controller.ts
@@ -0,0 +1,34 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+} from '@nestjs/common'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+
+@Controller('/questions/comments/:id')
+export class DeleteQuestionCommentController {
+  constructor(private deleteQuestionComment: DeleteQuestionCommentUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') questionCommentId: string,
+  ) {
+    const userId = user.sub
+
+    const result = await this.deleteQuestionComment.execute({
+      questionCommentId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      console.log(result.value)
+      throw new BadRequestException()
+    }
+  }
+}
