4832605f0f7536f8473780a6ace996e38f4ca055
Luana Vefago dos Santos <luana.santos@dynamox.net>
2025-01-14 09:37:45 -0300

    adjust: fix failing tests

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -5,9 +5,9 @@ import {
   AnswerComment,
   AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
 
 export function makeAnswerComment(
   override: Partial<AnswerCommentProps> = {},
@@ -30,15 +30,13 @@ export function makeAnswerComment(
 export class AnswerCommentFactory {
   constructor(private prisma: PrismaService) {}
 
-  async makePrismaAnswerComment(
-    data: Partial<AnswerCommentProps> = {},
-  ): Promise<AnswerComment> {
+  async makePrismaAnswerComment(data: Partial<AnswerCommentProps> = {}) {
     const answerComment = makeAnswerComment(data)
 
-    await this.prisma.comment.create({
+    const answerCommentCreated = await this.prisma.comment.create({
       data: PrismaAnswerCommentMapper.toPrisma(answerComment),
     })
 
-    return answerComment
+    return answerCommentCreated
   }
 }
