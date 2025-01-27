332eedea7f8b1f18b9d8918e86983708a2f8fb95
luanavfg <luanavefago1@gmail.com>
2025-01-27 09:49:50 -0300

    adjust: adjust use cases and test

--- test/factories/make-answer-attachment.ts
+++ test/factories/make-answer-attachment.ts
@@ -3,6 +3,8 @@ import {
   AnswerAttachment,
   AnswerAttachmentProps,
 } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswerAttachment(
   override: Partial<AnswerAttachmentProps> = {},
